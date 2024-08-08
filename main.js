c72de9681f71d6afd94c3d1b134824302d15bd1e
yesbusdriver <ypark951118@gmail.com>
2024-08-08 18:07:41 +0900

    redo

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-import config from ".apiKey"
+import config from './apikey.js'
 const API_KEY= config.MY_API_KEY
-const getLatestNews = ()=>{
-    const url = new URL (`https://newsapi.org/v2/top-headlines?country=us&apiKey= ${API_KEY}`) ;
-    console.log("uuu", url);
-}
+
+let news = []
+
+const getLatestNews = async ()=>{
+    //new URL 은 url instance를 만들어 준다. 객체를 만들어줌
+    const url = new URL (`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`) ;
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    news = data.articles
+    console.log("dddd",news);
+};
 
 getLatestNews();
 
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
 
